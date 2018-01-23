7d7e1cdb803b53459805102064feb4d123844825
jasapper <jasap98@yahoo.com>
2018-01-23 00:42:25 -0500

    Trying to fix heroku app crash

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -16,7 +16,7 @@ router.get("/burgers", function (req, res) {
 });
 
 router.post("/burgers/create", function(req, res){
-	console.log(req.body.burger_name, " =entered name for burger")
+	console.log(req.body.burger_name, " = name entered for new burger")
 	burger.create("burger_name", req.body.burger_name, function(){
 		res.redirect("/burgers");
 	});
